bb6f5803a4d9956b971425406fef13ad9946ff8b
PRAVIN RAJ <pravinjordan023@gmail.com>
2026-01-18 13:08:54 +0530

    fix: for

--- backend/config/db.php
+++ backend/config/db.php
@@ -11,7 +11,9 @@ class DB {
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
         $dotenv->load();
 
-        $uri = getenv('MONGO_URI') ?: "mongodb://localhost:27017";
+        // $uri = getenv('MONGO_URI') ?: "mongodb://localhost:27017";
+        // DEBUG: Force Prod URI to bypass .env issue
+        $uri = "mongodb+srv://pravinjordan023:Ravipriya_023@farmer-cluster.talcjux.mongodb.net/farmerDB?retryWrites=true&w=majority&appName=farmer-cluster";
         $dbName = getenv('MONGO_DB') ?: "farmerDB";
 
         try {
